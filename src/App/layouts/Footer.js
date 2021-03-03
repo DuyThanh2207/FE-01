281f4c5108fdd6b1c95715969fea1ab683bf9c55
DuyThanh2207 <td.thanh2207@gmail.com>
2021-03-03 11:58:07 +0700

    banner & reponsive header

--- src/App/layouts/Footer.js
+++ src/App/layouts/Footer.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <p>Liên hệ với chúng tôi</p>
+            <ul>
+              <li>
+                Tầng 4, tòa nhà Hanoi Group Số 442 Đội Cấn, P.Cống Vị, Q. Ba
+                Đình, Hà Nội
+              </li>
+              <li>
+                <p>(04) 6674 2332</p>
+                <p>(04) 3786 8904</p>
+              </li>
+              <li>
+                <p>(04) 6680 9686</p>
+                Support&#64;bizweb.vn
+              </li>
+            </ul>
+          </div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/App/layouts/Header.js
+++ src/App/layouts/Header.js
@@ -1,129 +1,230 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/logo.png";
 import product from "../../assets/images/product.png";
 function Header() {
   return (
-    <div className="header d-flex align-items-center">
-      <div className="logo">
-        <img src={logo} alt="" />
-      </div>
-      <ul>
-        <li>
-          <a href="">TRANG CHỦ</a>
-        </li>
-        <li>
-          <a href="">GIỚI THIỆU</a>
-        </li>
-        <li>
-          <div class="dropdown">
-            <button class="dropbtn">
-              SẢN PHẨM
-              <i className="fa fa-chevron-down"></i>
-            </button>
-            <div class="dropdown-content">
-              <div className="triangle"></div>
-              <div className="row navbar-content">
-                <div className="col-4 navbar-item">
-                  <div className="header-content">DƯỠNG DA</div>
-                  <hr />
-                  <div className="item-content">Áo khoác</div>
-                  <div className="item-content">Bộ áo liền quần</div>
-                  <div className="item-content">Dresses</div>
-                  <div className="item-content">Quần / Váy</div>
-                  <div className="item-content">Quần short</div>
-                  <div className="item-content">Quần jean</div>
-                  <div className="item-content">Đồ đan</div>
-                  <div className="item-content">Áo nỉ</div>
-                </div>
-                <div className="col-4 navbar-item">
-                  <div className="header-content">Nước hoa</div>
-                  <hr />
-                  <div className="item-content">Áo cánh / Áo sơ mi</div>
-                  <div className="item-content">Túi xách &amp; Balô</div>
-                  <div className="item-content">Khăn quàng cổ và Foulards</div>
-                  <div className="item-content">Dệt kim Hats &amp; Gloves</div>
-                  <div className="item-content">Vớ &amp; Socks</div>
-                  <div className="item-content">Jewerly</div>
-                  <div className="item-content">Bị bảo vệ</div>
-                  <div className="item-content">Giày</div>
-                </div>
-                <div className="col-4 navbar-item">
-                  <div className="header-content">Trang sức</div>
-                  <hr />
-                  <div className="item-content">Áo cánh / Áo sơ mi</div>
-                  <div className="item-content">Túi xách &amp; Balô</div>
-                  <div className="item-content">Khăn quàng cổ và Foulards</div>
-                  <div className="item-content">Dệt kim Hats &amp; Gloves</div>
-                  <div className="item-content">Vớ &amp; Socks</div>
-                  <div className="item-content">Jewerly</div>
-                  <div className="item-content">Bị bảo vệ</div>
-                  <div className="item-content">Giày</div>
-                </div>
-              </div>
+    <div className="header">
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col-auto">
+            <div className="header__logo">
+              <img src={logo} alt="" />
             </div>
           </div>
-        </li>
-        <li>
-          <a href="">TIN TỨC</a>
-        </li>
-        <li>
-          <a href="">BẢN ĐỒ</a>
-        </li>
-        <li>
-          <a href="">LIÊN HỆ</a>
-        </li>
-      </ul>
-      <div class="dropdown">
-        <button class="dropbtn-cart">
-          <i className="fa fa-shopping-cart"></i>
-        </button>
-        <div class="dropdown-content-cart">
-          <div className="triangle-cart"></div>
-          <div className="row product">
-            <div className="col-6">
-              <img src={product} alt="" />
-            </div>
-            <div className="col-5 product-details">
-              <span>Áo sơ mi Nam</span>
-              <br />
-              <span className="price">
-                120.000<span className="price-icon">đ</span>
-              </span>
-            </div>
-            <div className="col-1 remove-product">
-              <i className="fa fa-plus"></i>
-            </div>
-          </div>
-          <div className="row product mt-3 d-flex">
-            <div className="col-6">
-              <img src={product} alt="" />
-            </div>
-            <div className="col-5 product-details">
-              <span>Áo sơ mi Nam</span>
-              <br />
-              <span className="price">
-                120.000<span className="price-icon">đ</span>
-              </span>
-            </div>
-            <div className="col-1 remove-product">
-              <i className="fa fa-plus"></i>
-            </div>
-          </div>
-          <hr />
           <div
-            className="d-flex justify-content-between"
-            style={{ margin: "10px" }}
+            className="col-auto"
+            
           >
-            <span className="total">Tổng số</span>
-            <div className="price-total">
-              240.000<span className="price-icon">đ</span>
-            </div>
+            <ul className="header__nav-list" >
+              <li>
+                <a href="#">TRANG CHỦ</a>
+              </li>
+              <li>
+                <a href="#">GIỚI THIỆU</a>
+              </li>
+              <li className="header__nav-list__dropdown">
+                <a href="#">
+                  SẢN PHẨM
+                  <i className="fa fa-chevron-down"></i>
+                </a>
+                <ul className="header__nav-list__dropdownlist">
+                  <div className="header__nav-list__dropdown__triangle"></div>
+                  <div className="row">
+                    <div className="col-sm-12 col-md-4">
+                      <p className="header__nav-list__dropdownlist__title">
+                        DƯỠNG DA
+                      </p>
+                      <ul className="header__nav-list__dropdownlist__details">
+                        <li>
+                          <a>Áo khoác</a>
+                        </li>
+                        <li>
+                          <a>Bộ áo liền quần</a>
+                        </li>
+                        <li>
+                          <a>Dresses</a>
+                        </li>
+                        <li>
+                          <a>Quần / Váy</a>
+                        </li>
+                        <li>
+                          <a>Quần short</a>
+                        </li>
+                        <li>
+                          <a>Quần jean</a>
+                        </li>
+                        <li>
+                          <a>Đồ đan</a>
+                        </li>
+                        <li>
+                          <a>Áo nỉ</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="col-sm-12 col-md-4">
+                      <p className="header__nav-list__dropdownlist__title">
+                        NƯỚC HOA
+                      </p>
+                      <ul className="header__nav-list__dropdownlist__details">
+                        <li>
+                          <a>Áo khoác</a>
+                        </li>
+                        <li>
+                          <a>Bộ áo liền quần</a>
+                        </li>
+                        <li>
+                          <a>Dresses</a>
+                        </li>
+                        <li>
+                          <a>Quần / Váy</a>
+                        </li>
+                        <li>
+                          <a>Quần short</a>
+                        </li>
+                        <li>
+                          <a>Quần jean</a>
+                        </li>
+                        <li>
+                          <a>Đồ đan</a>
+                        </li>
+                        <li>
+                          <a>Áo nỉ</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="col-sm-12 col-md-4">
+                      <p className="header__nav-list__dropdownlist__title">
+                        TRANG SỨC
+                      </p>
+                      <ul className="header__nav-list__dropdownlist__details">
+                        <li>
+                          <a>Áo khoác</a>
+                        </li>
+                        <li>
+                          <a>Bộ áo liền quần</a>
+                        </li>
+                        <li>
+                          <a>Dresses</a>
+                        </li>
+                        <li>
+                          <a>Quần / Váy</a>
+                        </li>
+                        <li>
+                          <a>Quần short</a>
+                        </li>
+                        <li>
+                          <a>Quần jean</a>
+                        </li>
+                        <li>
+                          <a>Đồ đan</a>
+                        </li>
+                        <li>
+                          <a>Áo nỉ</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </ul>
+              </li>
+              <li>
+                <a href="#">TIN TỨC</a>
+              </li>
+              <li>
+                <a href="#">BẢN ĐỒ</a>
+              </li>
+              <li>
+                <a href="#">LIÊN HỆ</a>
+              </li>
+            </ul>
           </div>
-          <br />
-          <button className="btn_cart">GIỎ HÀNG</button>
+          <div className="col-auto" style={{ padding: "0" }}>
+            <ul className="header__function-list">
+              <li className="header__function-list__dropdown">
+                <i className="fa fa-shopping-cart"></i>
+                <ul className="header__function-list__dropdownlist">
+                  <div className="header__function-list__dropdown__triangle"></div>
+                  <div className="header__cart__product-details">
+                    <div className="row">
+                      <div className="col-6">
+                        <img src={product} alt="" />
+                      </div>
+                      <div className="col-5 header__cart__product-details__product-name">
+                        <span>Áo sơ mi Nam</span>
+                        <br />
+                        <span className="header__cart__product-details__price">
+                          120.000
+                          <span className="header__cart__product-details__price-icon">
+                            đ
+                          </span>
+                        </span>
+                      </div>
+                      <div className="col-1">
+                        <div className="col-1 header__cart__product-details__remove-product">
+                          <i className="fa fa-plus"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <br />
+                    <div className="row">
+                      <div className="col-6">
+                        <img src={product} alt="" />
+                      </div>
+                      <div className="col-5 header__cart__product-details__product-name">
+                        <span>Áo sơ mi Nam</span>
+                        <br />
+                        <span className="header__cart__product-details__price">
+                          120.000
+                          <span className="header__cart__product-details__price-icon">
+                            đ
+                          </span>
+                        </span>
+                      </div>
+                      <div className="col-1">
+                        <div className="col-1 header__cart__product-details__remove-product">
+                          <i className="fa fa-plus"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="header__line">
+                      {" "}
+                      <hr />
+                    </div>
+                    <div
+                      className="d-flex justify-content-between"
+                      style={{ margin: "10px" }}
+                    >
+                      <span className="header__cart__product-details__total">
+                        Tổng số
+                      </span>
+                      <div className="header__cart__product-details__price-total">
+                        240.000
+                        <span className="header__cart__product-details__price-icon">
+                          đ
+                        </span>
+                      </div>
+                    </div>
+                    <br />
+                    <button className="header__cart__product-details__btn_cart">
+                      GIỎ HÀNG
+                    </button>
+                  </div>
+                </ul>
+              </li>
+              <li>
+                <i className="fa fa-search"></i>
+              </li>
+              <li
+                className="header__navbar-mobile"
+                
+              >
+                <div className="circle d-flex justify-content-center align-items-center">
+                  <i className="fa fa-bars"></i>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <i className="fa fa-search"></i>
     </div>
   );
 }
